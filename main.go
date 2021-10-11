da1de19dc4af83e7ac0683e646fe7d3c48c113b0
pradeep-selva <pradeepselvaraju@gmail.com>
2021-10-11 15:10:27 +0530

    ft: block number range calculations (2 hours)

--- main.go
+++ main.go
@@ -15,19 +15,26 @@ import (
 	AastraVault "github.com/pradeep-selva/aastra-put-vault-monitor/generated/aastra-vault"
 	"github.com/pradeep-selva/aastra-put-vault-monitor/utils"
 )
+const TIME_FOR_ONE_BLOCK = 13
+const TIME_RANGE = 2*60*60;
 
 type eventProcessor func()
+type BlockRange struct {
+	start *big.Int
+	end *big.Int
+}
 
 func readAndProcessLogs(
 	client *ethclient.Client,
 	envVarName string,
 	event string,
+	blockRange BlockRange,
 	processEvent eventProcessor,
 ) {
 	contractAddress := common.HexToAddress(utils.GetEnvVar(envVarName))
 	eventsQuery := ethereum.FilterQuery{
-		FromBlock: big.NewInt(13371034),
-		ToBlock:   big.NewInt(13396011),
+		FromBlock: blockRange.start,
+		ToBlock:   blockRange.end,
 		Addresses: []common.Address{contractAddress},
 	}
 
@@ -55,16 +62,25 @@ func main() {
 
 	utils.LogInfo("Connected to infura node")
 
+	header, err := client.HeaderByNumber(context.Background(), nil)
+    utils.CheckError(err, "Could not retrieve headers")
+
+    currentBlock := header.Number
+	// block from 2 hours ago
+	olderBlock := big.NewInt(0).Sub(currentBlock, big.NewInt(TIME_RANGE/TIME_FOR_ONE_BLOCK))
+
 	readAndProcessLogs(
 		client,
 		"AASTRA_VAULT_ADDRESS",
 		utils.Events.CompoundFee,
+		BlockRange{olderBlock, currentBlock},
 		func() { log.Println("CompoundFee") },
 	)
 	readAndProcessLogs(
 		client,
 		"ROUTER_ADDRESS",
 		utils.Events.Rebalance,
+		BlockRange{olderBlock, currentBlock},
 		func() { log.Println("Rebalance") },
 	)
 }
