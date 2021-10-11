144b5c596aed846a0e641eed3401c0373bf008e1
pradeep-selva <pradeepselvaraju@gmail.com>
2021-10-11 14:43:14 +0530

    rf: minor refactor

--- go.mod
+++ go.mod
@@ -4,5 +4,5 @@ go 1.15
 
 require (
 	github.com/ethereum/go-ethereum v1.10.9
-	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/joho/godotenv v1.4.0
 )
--- main.go
+++ main.go
@@ -16,7 +16,37 @@ import (
 	"github.com/pradeep-selva/aastra-put-vault-monitor/utils"
 )
 
-func main(){
+type eventProcessor func()
+
+func readAndProcessLogs(
+	client *ethclient.Client,
+	envVarName string,
+	event string,
+	processEvent eventProcessor,
+) {
+	contractAddress := common.HexToAddress(utils.GetEnvVar(envVarName))
+	eventsQuery := ethereum.FilterQuery{
+		FromBlock: big.NewInt(13371034),
+		ToBlock:   big.NewInt(13396011),
+		Addresses: []common.Address{contractAddress},
+	}
+
+	logs, err := client.FilterLogs(context.Background(), eventsQuery)
+	utils.CheckError(err, "Could not retrieve vault logs")
+
+	utils.LogInfo("Loaded contract and retrieved logs")
+
+	_, err = abi.JSON(strings.NewReader(string(AastraVault.AastraVaultABI)))
+	utils.CheckError(err, "Could not read contract ABI")
+
+	for _, vLog := range logs {
+		if vLog.Topics[0].Hex() == event {
+			processEvent()
+		}
+	}
+}
+
+func main() {
 	err := godotenv.Load(".env")
 	utils.CheckError(err, "Could not load .env")
 
@@ -24,43 +54,17 @@ func main(){
 	utils.CheckError(err, "Could not initialize eth client")
 
 	utils.LogInfo("Connected to infura node")
-	
-	vaultAddress := common.HexToAddress(utils.GetEnvVar("AASTRA_VAULT_ADDRESS"))
-	vaultEventsQuery := ethereum.FilterQuery{
-		FromBlock: big.NewInt(13371034),
-		ToBlock: big.NewInt(13396011),
-		Addresses: []common.Address{vaultAddress},
-	}
 
-	vaultLogs, err := client.FilterLogs(context.Background(), vaultEventsQuery)
-	utils.CheckError(err, "Could not retrieve vault logs")
-
-	utils.LogInfo("Loaded Vault contract and retrieved logs")
-
-	_, err = abi.JSON(strings.NewReader(string(AastraVault.AastraVaultABI)))
-	utils.CheckError(err, "Could not read contract ABI")
-
-	for _, vLog := range vaultLogs {
-		if vLog.Topics[0].Hex() == utils.Events.CompoundFee {
-			log.Println("CompoundFee")
-		}
-	}
-
-	routerAddress := common.HexToAddress(utils.GetEnvVar("ROUTER_ADDRESS"))
-	routerEventsQuery := ethereum.FilterQuery{
-		FromBlock: big.NewInt(13371034),
-		ToBlock: big.NewInt(13396011),
-		Addresses: []common.Address{routerAddress},
-	}
-
-	routerLogs, err := client.FilterLogs(context.Background(), routerEventsQuery)
-	utils.CheckError(err, "Could not retrieve router logs")
-
-	utils.LogInfo("Loaded Router contract and retrieved logs")
-
-	for _, vLog := range routerLogs {
-		if vLog.Topics[0].Hex() == utils.Events.Rebalance {
-			log.Println("Rebalance")
-		}
-	}
+	readAndProcessLogs(
+		client,
+		"AASTRA_VAULT_ADDRESS",
+		utils.Events.CompoundFee,
+		func() { log.Println("CompoundFee") },
+	)
+	readAndProcessLogs(
+		client,
+		"ROUTER_ADDRESS",
+		utils.Events.Rebalance,
+		func() { log.Println("Rebalance") },
+	)
 }
